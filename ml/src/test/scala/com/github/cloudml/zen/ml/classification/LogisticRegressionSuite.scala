81efd65839cb7d2db9540025cb620e4af71e1d8e
Peishen Jia <v-pej@microsoft.com>
2015-04-30 10:23:43 +0800

    details

--- ml/src/test/scala/com/github/cloudml/zen/ml/classification/LogisticRegressionSuite.scala
+++ ml/src/test/scala/com/github/cloudml/zen/ml/classification/LogisticRegressionSuite.scala
@@ -39,12 +39,11 @@ class LogisticRegressionSuite extends FunSuite with SharedSparkContext with Matc
     var i = 0
     val startedAt = System.currentTimeMillis()
     val (model, lossArr) = lr.run(maxIter)
-    lossArr.foreach(println)
     println((System.currentTimeMillis() - startedAt) / 1e3)
 
-//    lossArr.foreach(println)
-//    val ppsDiff = lossArr.init.zip(lossArr.tail).map { case (lhs, rhs) => lhs - rhs }
-//    assert(ppsDiff.count(_ > 0).toDouble / ppsDiff.size > 0.05)
-//    assert(lossArr.head - lossArr.last > 0)
+    lossArr.foreach(println)
+    val ppsDiff = lossArr.init.zip(lossArr.tail).map { case (lhs, rhs) => lhs - rhs }
+    assert(ppsDiff.count(_ > 0).toDouble / ppsDiff.size > 0.05)
+    assert(lossArr.head - lossArr.last > 0)
   }
 }
