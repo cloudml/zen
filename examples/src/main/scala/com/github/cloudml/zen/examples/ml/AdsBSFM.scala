916270f7867b39ed339acacbb1a65c8003cba76b
Guoqiang Li <witgo@qq.com>
2015-07-11 14:18:18 +0800

    add AdsBSFM.scala

---
+++ examples/src/main/scala/com/github/cloudml/zen/examples/ml/AdsBSFM.scala
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cloudml.zen.examples.ml
+
+import breeze.linalg.{SparseVector => BSV}
+import com.github.cloudml.zen.ml.recommendation._
+import com.github.cloudml.zen.ml.util.SparkHacker
+import org.apache.spark.graphx.GraphXUtils
+import org.apache.spark.mllib.linalg.{SparseVector => SSV}
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{Logging, SparkConf, SparkContext}
+import scopt.OptionParser
+
+object AdsBSFM extends Logging {
+
+  case class Params(
+    input: String = null,
+    out: String = null,
+    numIterations: Int = 200,
+    numPartitions: Int = -1,
+    stepSize: Double = 0.05,
+    regular: String = "0.01,0.01,0.01",
+    fraction: Double = 1.0,
+    rank: Int = 64,
+    useAdaGrad: Boolean = false,
+    useThreeViews: Boolean = false,
+    useWeightedLambda: Boolean = false,
+    diskOnly: Boolean = false,
+    kryo: Boolean = false) extends AbstractParams[Params]
+
+  def main(args: Array[String]) {
+    val defaultParams = Params()
+    val parser = new OptionParser[Params]("BSFM") {
+      head("AdsBSFM: an example app for BSFM.")
+      opt[Int]("numIterations")
+        .text(s"number of iterations, default: ${defaultParams.numIterations}")
+        .action((x, c) => c.copy(numIterations = x))
+      opt[Int]("numPartitions")
+        .text(s"number of partitions, default: ${defaultParams.numPartitions}")
+        .action((x, c) => c.copy(numPartitions = x))
+      opt[Int]("rank")
+        .text(s"dim of 2-way interactions, default: ${defaultParams.rank}")
+        .action((x, c) => c.copy(rank = x))
+      opt[Unit]("kryo")
+        .text("use Kryo serialization")
+        .action((_, c) => c.copy(kryo = true))
+      opt[Double]("stepSize")
+        .text(s"stepSize, default: ${defaultParams.stepSize}")
+        .action((x, c) => c.copy(stepSize = x))
+      opt[String]("regular")
+        .text(
+          s"""
+             |'r0,r1,r2' for SGD: r0=bias regularization, r1=1-way regularization,
+             |r2=2-way regularization, default: ${defaultParams.regular} (auto)
+           """.stripMargin)
+        .action((x, c) => c.copy(regular = x))
+      opt[Double]("fraction")
+        .text(
+          s"the sampling fraction, default: ${defaultParams.fraction}")
+        .action((x, c) => c.copy(fraction = x))
+      opt[Unit]("diskOnly")
+        .text("use DISK_ONLY storage levels")
+        .action((_, c) => c.copy(diskOnly = true))
+      opt[Unit]("adagrad")
+        .text("use AdaGrad")
+        .action((_, c) => c.copy(useAdaGrad = true))
+      opt[Unit]("threeViews")
+        .text("use three views")
+        .action((_, c) => c.copy(useThreeViews = true))
+      opt[Unit]("weightedLambda")
+        .text("use weighted lambda regularization")
+        .action((_, c) => c.copy(useWeightedLambda = true))
+      arg[String]("<input>")
+        .required()
+        .text("input paths")
+        .action((x, c) => c.copy(input = x))
+      arg[String]("<out>")
+        .required()
+        .text("out paths (model)")
+        .action((x, c) => c.copy(out = x))
+      note(
+        """
+          |For example, the following command runs this app on a synthetic dataset:
+          |
+          | bin/spark-submit --class com.github.cloudml.zen.examples.ml.AdsFM \
+          |  examples/target/scala-*/zen-examples-*.jar \
+          |  --rank 20 --numIterations 200 --regular 0.01 --kryo \
+          |  data/mllib/ads_data/*
+          |  data/mllib/FM_model
+        """.stripMargin)
+    }
+
+    parser.parse(args, defaultParams).map { params =>
+      run(params)
+    } getOrElse {
+      System.exit(1)
+    }
+  }
+
+  def run(params: Params): Unit = {
+    val Params(input, out, numIterations, numPartitions, stepSize, regular, fraction,
+    rank, useAdaGrad, useThreeViews, useWeightedLambda, diskOnly, kryo) = params
+    val storageLevel = if (diskOnly) StorageLevel.DISK_ONLY else StorageLevel.MEMORY_AND_DISK
+    val regs = regular.split(",").map(_.toDouble)
+    val l2 = (regs(0), regs(1), regs(2))
+    val checkpointDir = s"$out/checkpoint"
+    val conf = new SparkConf().setAppName(s"BSFM with $params")
+    if (kryo) {
+      GraphXUtils.registerKryoClasses(conf)
+      // conf.set("spark.kryoserializer.buffer.mb", "8")
+    }
+    val sc = new SparkContext(conf)
+    sc.setCheckpointDir(checkpointDir)
+    SparkHacker.gcCleaner(60 * 15, 60 * 15, "AdsBSFM")
+    val (trainSet, testSet, views) = if (useThreeViews) {
+      AdsUtils.genSamplesWithTimeAnd3Views(sc, input, numPartitions, fraction, storageLevel)
+    } else {
+      AdsUtils.genSamplesWithTime(sc, input, numPartitions, fraction, storageLevel)
+    }
+
+    val model = BSFM.trainClassification(trainSet, numIterations, stepSize, views, l2, rank,
+      useAdaGrad, useWeightedLambda, 1.0, storageLevel)
+    model.save(sc, out)
+    val auc = model.loss(testSet)
+    logInfo(f"Test AUC: $auc%1.4f")
+    println(f"Test AUC: $auc%1.4f")
+    sc.stop()
+  }
+
+}
